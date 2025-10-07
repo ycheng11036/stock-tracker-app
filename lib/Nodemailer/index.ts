89958b8cb24c0f2d1824873a442eb97e384f7b17
yao <ycheng11036@gmail.com>
2025-10-06 20:44:41 -0400

    Implement daily news summary

--- lib/Nodemailer/index.ts
+++ lib/Nodemailer/index.ts
@@ -1,5 +1,9 @@
 import nodemailer from "nodemailer";
-import { WELCOME_EMAIL_TEMPLATE } from "./templates";
+import {
+  NEWS_SUMMARY_EMAIL_TEMPLATE,
+  WELCOME_EMAIL_TEMPLATE,
+} from "./templates";
+import { string } from "better-auth";
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -24,6 +28,31 @@ export const sendWelcomeEmail = async ({
     to: email,
     subject: `Welcome to Signalist - your stock market toolkit is ready!`,
     text: "Thanks for joining Signalist",
+    html: htmlTemplate,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+export const sendNewsSummaryEmail = async ({
+  email,
+  date,
+  newsContent,
+}: {
+  email: string;
+  date: string;
+  newsContent: string;
+}): Promise<void> => {
+  const htmlTemplate = NEWS_SUMMARY_EMAIL_TEMPLATE.replace(
+    "{{date}}",
+    date
+  ).replace("{{newsContent}}", newsContent);
+
+  const mailOptions = {
+    from: `"Signalist News" <signalist@rainy.com>`,
+    to: email,
+    subject: `Market News Summary Today - ${date}`,
+    text: `Today's market news summary from Signalist`,
     html: htmlTemplate,
   };
 
