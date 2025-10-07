33580324176f806e5030619b710b736a92e71107
yao <ycheng11036@gmail.com>
2025-10-07 19:33:31 -0400

    use href to match instead of string

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -20,7 +20,7 @@ const NavItems = ({
   return (
     <ul className="flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium">
       {NAV_ITEMS.map(({ href, label }) => {
-        if (label === "Search") {
+        if (href === "/search") {
           return (
             <li key="search-trigger">
               <SearchCommand
